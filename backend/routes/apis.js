e7bf86d50e1d7e48ee46a583bd91c44e41bce40a
Abhishek Chaudhary <abhishekchaudhary0220@gmail.com>
2020-08-18 17:25:02 +0530

    edit and delete

--- backend/routes/apis.js
+++ backend/routes/apis.js
@@ -7,7 +7,7 @@ module.exports = function(ref){
         var description = req.body.description || "";
         var iscompleted = req.body.iscompleted || false;
         var isfavourite = req.body.isfavourite || false;
-        if(title){
+        if(title && (typeof(iscompleted) === "boolean") && (typeof(isfavourite) === "boolean")){
             ref.push({
                 title : title,
                 description: description,
@@ -22,17 +22,53 @@ module.exports = function(ref){
             });
         }
         else{
-            res.send("Pls send title");
+            res.send("Pls send title and other details in correct form");
         }
     });
     apis.route('/todo').get((req, res) => {
-        var id = req.query.id;
         ref.once("value", function(snapshot) {
-            if(id) res.send(snapshot.child(id).val());
-            else res.send(snapshot.val());
+            res.send(snapshot.val());
         }, function (errorObject) {
             res.send("The read failed: " + errorObject.code);
         }); 
+    });
+    apis.route('/todo/:id').get((req, res) => {
+        var id = req.params.id;
+        ref.once("value", function(snapshot) {
+            res.send(snapshot.child(id).val());
+        }, function (errorObject) {
+            res.send("The read failed: " + errorObject.code);
+        }); 
+    });
+    apis.route('/todo/:id/edit').post((req, res) => {
+        var new_todo = {};
+        var req_body = req.body;
+        var arr = ['title', 'description', 'iscompleted', 'isfavourite'];
+        for(var key in req_body){
+            if(arr.indexOf(key) != -1){
+                new_todo[key] = req_body[key]
+            }
+        }
+        if((new_todo.iscompleted == null || (typeof(new_todo.iscompleted) === "boolean")) && (new_todo.isfavourite == null || (typeof(new_todo.isfavourite) === "boolean") ) ){
+            var id = req.params.id
+            ref.child(id).update(new_todo, function(error) {
+                if (error) {
+                    res.send("Data could not be saved." + error);
+                } else {
+                    res.send("Data saved successfully.");
+                }
+            });
+        }
+        else res.send("Not valid body")       
+    });
+    apis.route('/todo/:id/edit').delete((req, res) => {
+        var id = req.params.id;
+        ref.child(id).remove().then( () => {
+            res.send({ status: 'ok' });
+        }).catch(function(error) {
+            console.log('Error deleting data:', error);
+            res.send({ status: 'error', error: error });
+        });
     });
     return apis
 }
